5814a6c07a579da960c2bf13981918dd0b99fecb
Simeon <simeons2002@gmail.com>
2023-02-26 14:55:52 +0100

    Add functionality to sketching(eraser, clear, choose col, random col)
    Added some functionality to the grid.
    BUG TO FIX: try to make the drawing to only start when a grid element is
    hovered over + the mouse button clicked down.

--- dynamic.js
+++ dynamic.js
@@ -1,9 +1,10 @@
 // Generating 16 by 16 grid
 function generateGrid() {
   let gridSize;
-  do {
-    gridSize = Number(prompt('what grid size do you want to have?'));
-  } while (gridSize > 100);
+  // do {
+  //   gridSize = Number(prompt('what grid size do you want to have?'));
+  // } while (gridSize > 100);
+  gridSize = 16; //DELETE THIS AFTERWARDS THIS IS JUST FOR CONVENIENCE TO WORK ON THE GRID ITSELF!N
   const grid = document.querySelector('.container');
   for (let i = 0; i < gridSize; i++) {
     let gridRow = document.createElement('div');
@@ -25,20 +26,65 @@ function displayGrid(element, color) {
 }
 
 // Change styling to black for grid element this is hovered over.
-function convertColorOnHover(grid) {
-  const gridRows = grid.children;
-  for (let row of gridRows) {
-    gridRowElements = Array.from(row.children);
-    //adding event listener to every element
-    gridRowElements.forEach((element) => {
-      element.addEventListener(
-        'mouseover',
-        (event) => (element.style.backgroundColor = 'black')
-      );
+function ChangeBGColorOnHover(grid, color) {
+  grid.addEventListener('mousedown', () => {
+    const gridRows = grid.children;
+    for (let row of gridRows) {
+      let gridRowElements = Array.from(row.children);
+      //adding event listener to every element
+      gridRowElements.forEach((element) => {
+        element.addEventListener('mouseover', (event) => {
+          element.style.backgroundColor = color;
+        });
+      });
+    }
+  });
+  grid.addEventListener('mouseup', () => {
+    let gridRowElements = Array.from(grid.children);
+    console.log(gridRowElements);
+  });
+}
+
+function clearGrid() {
+  let gridRows = Array.from(grid.children);
+  gridRows.forEach((gridRow) => {
+    Array.from(gridRow.children).forEach((gridRowElement) => {
+      gridRowElement.style.backgroundColor = 'transparent';
     });
-  }
+  });
+}
+
+function randomColor() {
+  const rValue = Math.floor(Math.random() * 256);
+  const gValue = Math.floor(Math.random() * 256);
+  const bValue = Math.floor(Math.random() * 256);
+  const aValue = (Math.random() * 1).toFixed(1);
+  const color = `rgba(${rValue}, ${gValue}, ${bValue}, ${aValue})`;
+  console.log(color); //checking the rgba values in console log.
+  ChangeBGColorOnHover(grid, color);
 }
 
 // MAIN PROGRAM
+const grid = generateGrid();
+const eraseColorBtn = document.querySelector('#erase-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const colorBtn = document.querySelector('#color-btn');
+const colorInput = document.querySelector('#color-input');
+const randomBtn = document.querySelector('#random-btn');
 
-convertColorOnHover(generateGrid());
+ChangeBGColorOnHover(grid, 'black'); //standard color is black.
+//erase styling of grid element.
+eraseColorBtn.addEventListener('click', (e) => {
+  ChangeBGColorOnHover(grid, 'transparent');
+});
+// clear whole grid
+clearBtn.addEventListener('click', clearGrid);
+
+// choose your own color
+colorBtn.addEventListener('click', () => colorInput.click());
+colorInput.addEventListener('input', (e) => {
+  let color = e.target.value;
+  ChangeBGColorOnHover(grid, color);
+});
+
+randomBtn.addEventListener('click', randomColor);
